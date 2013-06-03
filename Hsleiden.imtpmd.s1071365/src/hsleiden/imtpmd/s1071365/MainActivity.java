01c8cceb159c5a5ebf4c5945b81e36875a6cb9fb
Lisaaaaaaaj <li-sa-u@hotmail.com>
2013-06-03 21:00:58 +0200

    3e commit

--- Hsleiden.imtpmd.s1071365/src/hsleiden/imtpmd/s1071365/MainActivity.java
+++ Hsleiden.imtpmd.s1071365/src/hsleiden/imtpmd/s1071365/MainActivity.java
@@ -3,21 +3,68 @@ package hsleiden.imtpmd.s1071365;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
+import java.util.ArrayList;
+import java.util.List;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends Activity
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener
 {
-
-	@Override
+	SettingsFragment settingsFragment;
+	WeekFragment weekFragment;
+	PagerAdapter pagerAdapter;
+	ViewPager viewPager;
+	
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
-		 Intent intent = new Intent();
-	     intent.setClass(getApplicationContext(), AsyncTaskEvenementBegin.class);
-	     startActivity(intent);
-	}
+		 //opstellen van de actionbar
+		final ActionBar ab = getActionBar();
+		ab.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		
+		//fragments worden hier aangemaakt a.d.h.v. een eigen adapter
+		//data en interface worden aan elkaar gekoppeld door de klasse PagerAdapter
+		List <Fragment> fl = new ArrayList<Fragment>();
+		this.settingsFragment = new SettingsFragment();
+		fl.add(settingsFragment);
+		this.weekFragment = new WeekFragment();
+		fl.add(weekFragment);
+		
+		pagerAdapter = new PagerAdapter(getSupportFragmentManager(), fl);
+		
+		//viewpager toont fragments op scherm, gekoppeld aan PagerAdapter
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		viewPager.setAdapter(pagerAdapter);
+		
+		//door de OnpageListener worden de tabs verwisseld d.m.v. swipe
+		//de OnPageListener is een onderdeel van de viewPager
+		viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener()
+		{
+			public void onPageSelected(int positie)
+			{
+				ab.setSelectedNavigationItem(positie);
+			}
+		}
+		);
+		
+		//het instellen van de tabbladen, waartussen gewisseld kan worden
+		Tab settingsFragment = ab.newTab();
+		settingsFragment.setText("Settings Tab");
+		settingsFragment.setTabListener(this);
+		ab.addTab(settingsFragment);
+		
+		Tab weekFragment = ab.newTab();
+		weekFragment.setText("Week Tab");
+		weekFragment.setTabListener(this);
+		ab.addTab(settingsFragment);
+	} 
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
@@ -25,6 +72,27 @@ public class MainActivity extends Activity
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft)
+	{
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- Hsleiden.imtpmd.s1071365/src/hsleiden/imtpmd/s1071365/SettingsFragment.java
+++ Hsleiden.imtpmd.s1071365/src/hsleiden/imtpmd/s1071365/SettingsFragment.java
@@ -1,6 +1,67 @@
 package hsleiden.imtpmd.s1071365;
 
-public class SettingsFragment
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SettingsFragment extends Fragment implements OnClickListener
 {
+
+	public View rv; 
+	private ServerCommunicator sc;
+	
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+	
+	public View onCreateView(LayoutInflater ly, ViewGroup vg, Bundle savedInstanceState)
+	{
+		rv = ly.inflate(R.layout.fragment_main_settings, vg, false);
+		
+		Button bt = (Button) rv.findViewById(R.id.buttonverzend);
+		bt.setOnClickListener(this);
+		
+		return rv;
+		
+	}
+	
+	@Override
+	public void onClick(View arg0)
+	{
+		// TODO Auto-generated method stub
+		//gegevens uit de UI halen
+		EditText editTxtNaam = (EditText) rv.findViewById(R.id.edittxtnaam);
+		String name = editTxtNaam.getText().toString();
+		
+		EditText editTxtVraag = (EditText) rv.findViewById(R.id.edittxtvraag);
+		String question = editTxtVraag.getText().toString();
+		
+		EditText editTxtIp = (EditText) rv.findViewById(R.id.edittxtip);
+		String ipAdres = editTxtIp.getText().toString();
+		
+		EditText editTxtPort = (EditText) rv.findViewById(R.id.edittxtport);
+		int poort = Integer.parseInt(editTxtPort.getText().toString());
+		
+		//nieuwe verbinding maken met de server
+		this.setSc(new ServerCommunicator(this, name, question, ipAdres, poort));
+		
+		
+	}
+
+	public ServerCommunicator getSc()
+	{
+		return sc;
+	}
+
+	public void setSc(ServerCommunicator sc)
+	{
+		this.sc = sc;
+	}
 
 }
